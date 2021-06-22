3f39ee52c20a6c95f99a48ecfd901bece8340eac
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2021-06-22 12:15:34 +0200

    validate terraform file

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "melkio-tfstate"
+    storage_account_name = "melkiotfstate"
+    container_name       = "tfstate"
+    key                  = "github.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "group-with-terraform-and-github-actions"
+  location = "West Europe"
+}
