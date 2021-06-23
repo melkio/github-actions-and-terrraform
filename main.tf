cbb149aaefbab016eb0c509132782f864801917c
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2021-06-23 16:07:43 +0200

    add azure ad provider

--- main.tf
+++ main.tf
@@ -11,6 +11,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=2.46.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "1.4.0"
+    }
   }
 }
 
@@ -18,7 +22,24 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
-  location = "West Europe"
+provider "azuread" {
 }
+
+resource "azuread_application" "main" {
+  display_name = "demo1234"
+}
+
+resource "azuread_service_principal" "main" {
+  application_id = azuread_application.main.application_id
+}
+
+resource "random_string" "password" {
+  length = 32
+}
+
+resource "azuread_service_principal_password" "main" {
+  service_principal_id = azuread_service_principal.main.id
+  value                = random_string.password.result
+  end_date_relative    = "8760h"
+}
+
