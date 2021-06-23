71fd5080784cd467dcc8ed34eb75793a16bfa0ac
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2021-06-23 16:38:18 +0200

    add sp name as variable

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ provider "azuread" {
 }
 
 resource "azuread_application" "main" {
-  display_name = "demo1234"
+  display_name = var.sp_name
 }
 
 resource "azuread_service_principal" "main" {
--- variables.tf
+++ variables.tf
@@ -1,2 +1,5 @@
 variable "resource_group_name" {
 }
+
+variable "sp_name" {
+}
