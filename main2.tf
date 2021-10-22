34686997308bdbd21c971f4cae101ce35ffed73a
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2021-10-22 16:16:42 +0200

    add storage account

--- main2.tf
+++ main2.tf
@@ -22,3 +22,11 @@ resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = "West Europe"
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "melkio123123"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
