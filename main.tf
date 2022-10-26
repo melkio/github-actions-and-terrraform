1291866f9cd304be972986cce95aa8cf7e42dabe
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2022-10-26 17:33:20 +0200

    add demo1 storage account

--- main.tf
+++ main.tf
@@ -21,6 +21,14 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "container" {
-    name = "CloudDay2022"
-    location = "West Europe"
+  name     = "CloudDay2022"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "demo1" {
+  resource_group_name      = azurerm_resource_group.container.name
+  name                     = "demo1cloudday22"
+  location                 = azurerm_resource_group.container.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 }
